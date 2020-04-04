e5cdf18afd3cd987fc84e5cf7cf8e8a6bbc67e6c
Ayaz Qureshi <qureshi.ayaz29@gmail.com>
2020-04-04 22:40:13 +0530

    new login screen added

---
+++ app/src/main/java/in/insideandroid/loginscreen/Login2Activity.java
@@ -0,0 +1,14 @@
+package in.insideandroid.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
